c3219864cfbf4bbb691c9b820bbb005e929db6a6
Neel Bagora <neelbagora@google.com>
2020-06-29 23:16:31 +0000

    Add preset form submission and style page.

--- DSA-Campaign-Uplift-Estimation/src/main/webapp/Create/create-script.js
+++ DSA-Campaign-Uplift-Estimation/src/main/webapp/Create/create-script.js
@@ -14,5 +14,36 @@
 
 /* Submission form only requires 2 decimals, this function enforces that rule */
 function setTwoNumberDecimal(event) {
-    this.value = parseFloat(this.value).toFixed(2);
+  this.value = parseFloat(this.value).toFixed(2);
+}
+
+
+function submitPresetData() {
+  let xmlhttp= window.XMLHttpRequest ?
+    new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.status === 0) {
+      console.log(xmlhttp.status);
+      alert("Preset saved!");
+    }
+    else if ((xmlhttp.status < 200) && (xmlhttp.status >= 400)) {
+      alert("Error: Preset cannot be saved. Please try again later.");
+    }
+  }
+
+  // dynamically build a URL string with form elements
+  var keyvalpairs = [];
+  var form = document.getElementById('campaign-form'); // get the form somehow
+  for (var i = 0; i < form.elements.length; i++) {
+    var curr_element = form.elements[i];
+    keyvalpairs.push(encodeURIComponent(curr_element.name) + "=" + encodeURIComponent(curr_element.value));
+  }
+
+  // divide each parameter with '&'
+  var queryString = keyvalpairs.join("&");
+  
+  xmlhttp.open("POST", '/preset', true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send(queryString);
 }
