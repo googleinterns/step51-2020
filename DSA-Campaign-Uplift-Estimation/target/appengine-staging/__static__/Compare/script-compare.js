b9ccbd09f27700522e3ee4ac136b80bb8032ac45
Nevin George <nevingeorge@google.com>
2020-06-30 20:11:50 +0000

    Create index.yaml to fix datastore issue.

--- DSA-Campaign-Uplift-Estimation/target/appengine-staging/__static__/Compare/script-compare.js
+++ DSA-Campaign-Uplift-Estimation/target/appengine-staging/__static__/Compare/script-compare.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+google.charts.load('current', {'packages':['bar', 'table']});
+
 function getKeywordCampaigns() {
 	const keywordCampaignList = document.getElementById('keyword-campaigns');
     keywordCampaignList.innerHTML = '<option value=0>Select a keyword campaign</option>';
@@ -29,10 +31,59 @@ function getDSACampaigns() {
     DSACampaignsList.innerHTML = '<option value=0>Select a DSA campaign</option>';
     var keywordCampaignId = document.getElementById("keyword-campaigns").value;
 
-    fetch('/DSA-campaigns?keywordCampaignId=' + keywordCampaignId).then(response => response.json()).then(DSACampaigns => {
-        DSACampaigns.forEach(DSACampaign => {
-            DSACampaignsList.innerHTML += '<option value=' + DSACampaign.DSACampaignId + '>' + DSACampaign.name + '</option>';
+    if (keywordCampaignId != 0) {
+        fetch('/DSA-campaigns?keywordCampaignId=' + keywordCampaignId).then(response => response.json()).then(DSACampaigns => {
+            DSACampaigns.forEach(DSACampaign => {
+                DSACampaignsList.innerHTML += '<option value=' + DSACampaign.DSACampaignId + '>' + DSACampaign.name + '</option>';
+            });
         });
-    });
-    console.log('Got DSA campaigns.');
+        console.log('Got DSA campaigns.');
+    }
+}
+
+function drawBarGraph() {
+    var DSACampaignId = document.getElementById("DSA-campaigns").value;
+
+    if (DSACampaignId != 0) {
+        fetch('/DSA-campaign-id?DSACampaignId=' + DSACampaignId).then(response => response.json()).then(DSACampaign => {
+            fetch('/keyword-campaign-id?keywordCampaignId=' + DSACampaign.keywordCampaignId).then(response => response.json()).then(keywordCampaign => {
+                var data = google.visualization.arrayToDataTable([
+                    ['Statistic', keywordCampaign.name, DSACampaign.name],
+                    ['Impressions', keywordCampaign.impressions, DSACampaign.impressions],
+                    ['Clicks', keywordCampaign.clicks, DSACampaign.clicks],
+                    ['Cost (USD)', keywordCampaign.cost, DSACampaign.cost]
+                ]);
+
+                var options = {
+                    chart: {
+                        title: 'Statistics',
+                        subtitle: 'Impressions, Clicks, and Cost (USD)',
+                    },
+                    bars: 'horizontal' // Required for Material Bar Charts.
+                };
+
+                var chart = new google.charts.Bar(document.getElementById('bar-chart'));
+
+                chart.draw(data, google.charts.Bar.convertOptions(options));
+
+                drawTable(DSACampaign, keywordCampaign);
+            });
+        });
+
+        console.log("Drew bar graph.");
+    }
+}
+
+function drawTable(DSACampaign, keywordCampaign) {
+    var data = google.visualization.arrayToDataTable([
+        ['', 'name', 'start date', 'end date', 'daily budget', 'location', 'domain', 'target', 'impressions', 'clicks', 'cost (USD)'],
+        ['Keyword Campaign', keywordCampaign.name, keywordCampaign.fromDate, keywordCampaign.toDate, keywordCampaign.dailyBudget, keywordCampaign.location, keywordCampaign.domain, keywordCampaign.target, keywordCampaign.impressions, keywordCampaign.clicks, keywordCampaign.cost],
+        ['DSA Campaign', DSACampaign.name, DSACampaign.fromDate, DSACampaign.toDate, DSACampaign.dailyBudget, DSACampaign.location, DSACampaign.domain, DSACampaign.target, DSACampaign.impressions, DSACampaign.clicks, DSACampaign.cost],
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+
+    console.log("Drew table.");
 }
