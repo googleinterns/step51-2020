79c7780600af98dcce299d68bf8309073d9978af
Neel Bagora <neelbagora@google.com>
2020-07-11 00:22:54 +0000

    Change test to directly test doPost of PresetServlet.java

--- DSA-Campaign-Uplift-Estimation/src/test/java/com/google/sps/PresetServletTest.java
+++ DSA-Campaign-Uplift-Estimation/src/test/java/com/google/sps/PresetServletTest.java
@@ -30,6 +30,7 @@ import org.junit.runners.JUnit4;
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -41,6 +42,8 @@ import org.mockito.MockitoAnnotations;
 import com.google.gson.Gson;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /*
  * Tests the doGet() and doPost() functions in DSACampaignsServlet.java.
@@ -73,52 +76,71 @@ public final class PresetServletTest {
   }
 
   /**
-   * presetServletdoPostTest works by first mimicking the behavior of PresetServlet.doPost().
-   * and verifying that it is able to successfully post data to the datastore.
+   * presetServletdoPostTest works by first creating a mock HttpServletRequest
+   * object and calling PresetServlet.doPost(). Following, the function has
+   * multiple assertEquals to verify that the datastore accurately stores
+   * the request data.
    */
   @Test
   public void presetServletdoPostTest() throws IOException, ServletException {
+
+    request = mock(HttpServletRequest.class);
+    
+    response = mock(HttpServletResponse.class);
+    
+    // variables used to test the method.
+    when(request.getParameter("DSACampaignId")).thenReturn("0");
+    when(request.getParameter("campaignStatus")).thenReturn("pending");
+    when(request.getParameter("userId")).thenReturn("0");
+    when(request.getParameter("keywordCampaignId")).thenReturn("0");
+    when(request.getParameter("presetId")).thenReturn("0");
+    when(request.getParameter("name")).thenReturn("Test Name");
+    when(request.getParameter("startDate")).thenReturn("2001-01-01");
+    when(request.getParameter("endDate")).thenReturn("2001-01-03");
+    when(request.getParameter("manualCPC")).thenReturn("0.01");
+    when(request.getParameter("dailyBudget")).thenReturn("34.00");
+    when(request.getParameter("locations")).thenReturn("California, United States of America");
+    when(request.getParameter("domain")).thenReturn("http://google.com");
+    when(request.getParameter("targets")).thenReturn("http://google.com/page1");
+    when(request.getParameter("adText")).thenReturn("Some text");
+    when(request.getParameter("cost")).thenReturn("0");
+    when(request.getParameter("impressions")).thenReturn("0");
+    when(request.getParameter("clicks")).thenReturn("0");
+
+    // verify that datastore is empty
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     assertEquals(0, datastore.prepare(new Query("PresetData")).countEntities(withLimit(10)));
-    Entity presetEntity = new Entity("PresetData"); 
-    String userId = "0";
-    String presetId = "0";
-    String name = "Test Name";
-    // date format - yyyy-mm-dd
-    String startDate = "2001-01-01";
-    String endDate = "2001-01-03";
-    double manualCPC = 0.01;
-    double dailyBudget = 34.00;
-    String locations = "California, United States of America";
-    String domain = "http://google.com";
-    String targets = "http://google.com/page1";
-    String adText = "Test ad text";
+    
+    new PresetServlet().doPost(request, response);
 
-    presetEntity.setProperty("userId", userId);
-    presetEntity.setProperty("presetId", presetId);
-
-    DSACampaign dsaCampaign = new DSACampaign("0", userId, "0", name, "pending", startDate, endDate, manualCPC, dailyBudget, locations, domain, targets, adText, 0, 0, 0);
-    Gson gson = new Gson();
-    String dsaCampaignData = gson.toJson(dsaCampaign);
-    presetEntity.setProperty("presetData", dsaCampaignData);
-    datastore.put(presetEntity);
+    // verify that there is a preset entry in datastore
     assertEquals(1, datastore.prepare(new Query("PresetData")).countEntities(withLimit(10)));
 
     Query query = new Query("PresetData");
     Entity entity = datastore.prepare(query).asSingleEntity();
+    
+    assertEquals(request.getParameter("userId"), entity.getProperty("userId"));
+    assertEquals(request.getParameter("presetId"), entity.getProperty("presetId"));
 
-    assertEquals(userId, entity.getProperty("userId"));
-    assertEquals(presetId, entity.getProperty("presetId"));
-    assertEquals(dsaCampaignData, entity.getProperty("presetData"));
+    // convert JSON data representing DSACampaign to DSACampaign object
+    Gson gson = new Gson();
+    String dsaCampaignData = (String) entity.getProperty("presetData");
     DSACampaign testDSACampaign = gson.fromJson(dsaCampaignData, DSACampaign.class);
-    assertEquals(dsaCampaign.name, testDSACampaign.name);
-    assertEquals(dsaCampaign.locations, testDSACampaign.locations);
-    assertEquals(dsaCampaign.startDate, testDSACampaign.startDate);
-    assertEquals(dsaCampaign.endDate, testDSACampaign.endDate);
-    assertEquals(dsaCampaign.manualCPC, testDSACampaign.manualCPC, 0.01);
-    assertEquals(dsaCampaign.dailyBudget, testDSACampaign.dailyBudget, 0.01);
-    assertEquals(dsaCampaign.domain, testDSACampaign.domain);
-    assertEquals(dsaCampaign.targets, testDSACampaign.targets);
-    assertEquals(dsaCampaign.adText, testDSACampaign.adText);
+
+    // verify that the data used to create the campaign is accurately sent and retrieved from datastore
+    assertEquals(request.getParameter("keywordCampaignId"), testDSACampaign.keywordCampaignId);
+    assertEquals(request.getParameter("DSACampaignId"), testDSACampaign.DSACampaignId);
+    assertEquals(request.getParameter("name"), testDSACampaign.name);
+    assertEquals(request.getParameter("locations"), testDSACampaign.locations);
+    assertEquals(request.getParameter("startDate"), testDSACampaign.startDate);
+    assertEquals(request.getParameter("endDate"), testDSACampaign.endDate);
+    assertEquals(Double.parseDouble(request.getParameter("manualCPC")), testDSACampaign.manualCPC, 0.00);
+    assertEquals(Double.parseDouble(request.getParameter("dailyBudget")), testDSACampaign.dailyBudget, 0.00);
+    assertEquals(request.getParameter("domain"), testDSACampaign.domain);
+    assertEquals(request.getParameter("targets"), testDSACampaign.targets);
+    assertEquals(request.getParameter("adText"), testDSACampaign.adText);
+    assertEquals(Integer.parseInt(request.getParameter("cost")), testDSACampaign.cost, 0);
+    assertEquals(Integer.parseInt(request.getParameter("impressions")), testDSACampaign.impressions, 0);
+    assertEquals(Integer.parseInt(request.getParameter("clicks")), testDSACampaign.clicks, 0);
   }
 }
