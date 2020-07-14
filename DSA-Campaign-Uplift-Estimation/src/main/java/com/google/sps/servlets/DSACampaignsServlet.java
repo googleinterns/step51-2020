3b5318fd3b62a883b63bba998361718fd2d09b7b
Nevin George <nevingeorge@google.com>
2020-07-14 13:58:57 +0000

    Create unique DSA campaign id function.

--- DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/servlets/DSACampaignsServlet.java
+++ DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/servlets/DSACampaignsServlet.java
@@ -63,8 +63,8 @@ public class DSACampaignsServlet extends HttpServlet {
         if (userService.isUserLoggedIn()) {
             String userId = userService.getCurrentUser().getUserId();
 
-            DSACampaign DSACampaignObject = new DSACampaign(request.getParameter("DSACampaignId"), userId, request.getParameter("keywordCampaignId"),
-                request.getParameter("name"), request.getParameter("campaignStatus"), request.getParameter("startDate"), request.getParameter("endDate"), 
+            DSACampaign DSACampaignObject = new DSACampaign(getDSACampaignId(), userId, request.getParameter("keywordCampaignId"),
+                request.getParameter("name"), "pending", request.getParameter("startDate"), request.getParameter("endDate"), 
                 Double.parseDouble(request.getParameter("manualCPC")), Double.parseDouble(request.getParameter("dailyBudget")), request.getParameter("locations"),
                 request.getParameter("domain"), request.getParameter("targets"), request.getParameter("adText"), Integer.parseInt(request.getParameter("impressions")),
                 Integer.parseInt(request.getParameter("clicks")), Double.parseDouble(request.getParameter("cost")));
@@ -124,5 +124,27 @@ public class DSACampaignsServlet extends HttpServlet {
         DSACampaignEntity.setProperty("cost", DSACampaignObject.cost);
 
         return DSACampaignEntity;
+    }
+
+    // Retrieves a unique DSA campaign id from datastore.
+    public static int getDSACampaignId() {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query query = new Query("numDSACampaigns");
+        Entity numDSACampaignsEntity = datastore.prepare(query).asSingleEntity();
+        int numDSACampaigns = 1;
+
+        if (numDSACampaignsEntity != null) {
+            // There are DSA campaigns in datastore, the numDSACampaignsEntity was already created.
+            numDSACampaigns = (int) ((long) numDSACampaignsEntity.getProperty("number"));
+            numDSACampaignsEntity.setProperty("number", ++numDSACampaigns);
+            datastore.put(numDSACampaignsEntity);
+        } else {
+            // There are no DSA campaigns in datastore - need to create numDSACampaignsEntity.
+            Entity newNumDSACampaignsEntity = new Entity("numDSACampaigns");
+            newNumDSACampaignsEntity.setProperty("number", numDSACampaigns);
+            datastore.put(newNumDSACampaignsEntity);
+        }
+
+        return numDSACampaigns;
     }
 }
