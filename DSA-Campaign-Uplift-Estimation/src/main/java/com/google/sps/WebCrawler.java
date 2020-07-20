1cfb8f9f6531563d348ab690338d1609b4db6d2d
Nevin George <nevingeorge@google.com>
2020-07-20 18:47:13 +0000

    Update README.md, handle related keywords.

--- DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/WebCrawler.java
+++ DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/WebCrawler.java
@@ -21,22 +21,19 @@ import org.jsoup.select.Elements;
 import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.ArrayList;
 
-// Implements all functions that require the use of jsoup.
+// Implements all of the functions that require the use of jsoup.
 public class WebCrawler {
 
     public static double getWebsiteFactor(Entity keywordCampaignEntity, Entity DSACampaignEntity) {
+        // use a hashset to avoid duplicate entries
         HashSet<String> recommendedLinks = getRecommendedLinks((String) DSACampaignEntity.getProperty("domain"));
 
         // add the target pages to the recommended links
         String[] targetPages = ((String) DSACampaignEntity.getProperty("targets")).split(",");
         for (String targetPage : targetPages) {
-            targetPage = targetPage.trim();
-
-            // avoid duplicate entries
-            if (!recommendedLinks.contains(targetPage)) {
-                recommendedLinks.add(targetPage);
-            }
+            recommendedLinks.add(targetPage.trim());
         }
 
         double sumOfPageFactors = 0;
@@ -52,7 +49,7 @@ public class WebCrawler {
 
     // Crawls all pages from the domain to a depth of 1 to create the recommended list of pages.
     public static HashSet<String> getRecommendedLinks(String domain) {
-        // use a hash set to easily deal with duplicate entries
+        // use a hash set to avoid duplicate entries
         HashSet<String> recommendedLinks = new HashSet<String>();
 
         // begin with the domain
@@ -66,12 +63,7 @@ public class WebCrawler {
             // parse the HTML to get the links to other URLs and add them to the hash set
             Elements pageLinks = document.select("a[href]");
             for (Element page : pageLinks) {
-                String url = page.attr("abs:href");
-                
-                // avoid duplicate entries
-                if (!recommendedLinks.contains(url)) {
-                    recommendedLinks.add(url);
-                }
+                recommendedLinks.add(page.attr("abs:href"));
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
@@ -81,7 +73,64 @@ public class WebCrawler {
     }
 
     public static double getPageFactor(String url) {
-        // TODO
-        return 1;
+        HashSet<String> keywords = new HashSet<String>();
+
+        // add the significant keywords of the url to the hash set
+        // split the url by [/ - . (blank space) _ :]
+        String[] urlElements = url.split("/|\\-|\\.|\\s|\\_|\\:");
+        for (String urlElement : urlElements) {
+            // add all keywords resembling the element to the hash set
+            ArrayList<String> relatedKeywords = getRelatedKeywords(urlElement);
+            for (String keyword : relatedKeywords) {
+                keywords.add(keyword);
+            }
+        }
+
+        try {
+            // get the page HTML
+            Document document = Jsoup.connect(url).get();
+
+            Elements pageLinks = document.select("title");
+
+            // TODO
+            
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    // Ensures that the keyword is significant, converts to lower case, and returns both the sigular and plural cases. 
+    public static ArrayList<String> getRelatedKeywords(String word) {
+        ArrayList<String> relatedKeywords = new ArrayList<String>();
+        word = word.trim().toLowerCase();
+
+        if (isSignificant(word)) {
+            relatedKeywords.add(word);
+
+            // add both the singular and plural forms
+            if (word.charAt(word.length()-1) == 's') {
+                relatedKeywords.add(word.substring(0, word.length()-1));
+            } else {
+                relatedKeywords.add(word += 's');
+            }
+        }
+        
+        return relatedKeywords;
+    }
+
+    // Checks if the word has meaningful content.
+    public static boolean isSignificant(String word) {
+        if (word.length() == 0) {
+            return false;
+        }
+        // common url elements
+        if (word.equals("https") || word.equals("www") || word.equals("com") || word.equals("org") || word.equals("html") || word.equals("php")) {
+            return false;
+        }
+        // articles
+        if (word.equals("the") || word.equals("a") || word.equals("an")) {
+            return false;
+        }
+        return true;
     }
 }
