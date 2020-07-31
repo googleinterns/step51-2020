abbfb7e031ef6c2c53a5f5c3b3e341994b3b0788
Neel Bagora <neelbagora@google.com>
2020-07-31 19:01:37 +0000

    Add hangouts bot servlet and communication between bot and servlets.

--- DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/servlets/DSACampaignsHangoutsServlet.java
+++ DSA-Campaign-Uplift-Estimation/src/main/java/com/google/sps/servlets/DSACampaignsHangoutsServlet.java
@@ -34,20 +34,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import com.google.sps.servlets.DSACampaignsServlet;
 
-@WebServlet('/DSA-campaigns-hangouts')
+@WebServlet("/DSA-campaigns-hangouts")
 public class DSACampaignsHangoutsServlet extends HttpServlet {
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      String userId = request.getParameter("userId");
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      Query query = new Query("DSACampaign").setFilter(new Query.FilterPredicate("userId", Query.FilterOperator.EQUAL, userId)).addSort("DSACampaignId", SortDirection.ASCENDING);
+    	PreparedQuery results = datastore.prepare(query);
+
+      ArrayList<DSACampaign> DSACampaigns = new ArrayList<DSACampaign>();
+      for (Entity entity : results.asIterable()) {
+          DSACampaigns.add(new DSACampaignsServlet().createDSACampaignFromEntity(entity));
+      }
+
+      Gson gson = new Gson();
+      String json = gson.toJson(DSACampaigns);
+      response.setContentType("application/json;");
+      response.getWriter().println(json);
+    }
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        System.out.println("POST request received from bot.");
         if (request.getParameter("delete") != null) {
             System.err.println("Inside If Parameter");
             String campaignId = request.getParameter("id");
-            deleteDSACampaign(campaignId);
+            new DSACampaignsServlet().deleteDSACampaign(campaignId);
             return;
         } else {
             // user ID represents user email
-            String userId = userService.getCurrentUser().getEmail();
+            String userId = request.getParameter("userId");
             DSACampaign DSACampaignObject = new DSACampaign(KeywordCampaignsServlet.getNewCampaignId(false), userId, request.getParameter("keywordCampaignId"),
                 request.getParameter("name"), "pending", request.getParameter("startDate"), request.getParameter("endDate"), 
                 Double.parseDouble(request.getParameter("manualCPC")), Double.parseDouble(request.getParameter("dailyBudget")), request.getParameter("locations"),
@@ -55,7 +76,7 @@ public class DSACampaignsHangoutsServlet extends HttpServlet {
                 0, 0, 0, null);
 
             DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-            datastore.put(createEntityFromDSACampaign(DSACampaignObject));
+            datastore.put(new DSACampaignsServlet().createEntityFromDSACampaign(DSACampaignObject));
 
         }
     }
